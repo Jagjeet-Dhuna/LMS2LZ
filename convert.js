1a2918e0eca2a346fd4decc1f541733b22e2e07a
Jagjeet Dhuna <dhunajagjeet@gmail.com>
2023-06-21 16:54:23 +0100

    Feature update

--- convert.js
+++ convert.js
@@ -15,18 +15,23 @@ function convertText() {
     lines.forEach(line => {
         
         let modified_line = line
-            .replace(/\*\*\[\`/g, '<span style="color: #3c5fde;">!!</span>')
-            .replace(/\`\]\(urn:gd:lg:a:send-vm-keys\)\*\*/g, '<span style="color: #3c5fde;">!!</span>')
-            .replace(/\*\*\[/g, '<span style="color: #3c5fde;">!!</span>')
-            .replace(/\]\(urn:gd:lg:a:send-vm-keys\)\*\*/g, '<span style="color: #3c5fde;">!!</span>')
-            .replace(/\(hyperVLab\)\.vm/g, '<span style="color: #3c5fde;">(' + hyperVDef + ').vm</span>')
-            .replace(/\.user\((\d+)\)\.username/g, '<span style="color: #3c5fde;">.credentials($1).username</span>')
-            .replace(/\.user\((\d+)\)\.password/g, '<span style="color: #3c5fde;">.credentials($1).password</span>')
-            .replace(/\(azure\)\.username/g, '<span style="color: #3c5fde;">(' + azureDef + ').credentials(0).username</span>')
-            .replace(/\(azure\)\.password/g, '<span style="color: #3c5fde;">(' + azureDef + ').credentials(0).password</span>')
-            .replace(/\(azure\)\.subscriptionName/g, '<span style="color: #3c5fde;">(' + azureDef + ').subscriptionName</span>')
-            .replace(/\(azure\)\.subscriptionId/g, '<span style="color: #3c5fde;">(' + azureDef + ').subscriptionId</span>')
-            .replace(/\$gd\.com\(azure\)\.resourceGroups\(/g, '$gd.com(<span style="color: #3c5fde;">' + azureDef + '</span>).resourceGroups(');
+            .replace(/\*\*\[\`/g, '<span style="color: #3c5fde;">!!</span>') // **[` = !!
+            .replace(/\`\]\(urn:gd:lg:a:send-vm-keys\)\*\*/g, '<span style="color: #3c5fde;">!!</span>') // `](urn:gd:lg:a:send-vm-keys)** = !!
+            .replace(/\*\*\[/g, '<span style="color: #3c5fde;">!!</span>') // **[ = !!
+            .replace(/\]\(urn:gd:lg:a:send-vm-keys\)\*\*/g, '<span style="color: #3c5fde;">!!</span>') // ](urn:gd:lg:a:send-vm-keys)** = !!
+            .replace(/\(hyperVLab\)\.vm/g, '<span style="color: #3c5fde;">(' + hyperVDef + ').vm</span>') // hyperVLab.vm = hyperVDef.vm
+            .replace(/\.user\((\d+)\)\.username/g, '<span style="color: #3c5fde;">.credentials($1).username</span>') // .user(n).username = .credentials(n).username
+            .replace(/\.user\((\d+)\)\.password/g, '<span style="color: #3c5fde;">.credentials($1).password</span>') // .user(n).password = .credentials(n).password
+            .replace(/\(azure\)\.username/g, '<span style="color: #3c5fde;">(' + azureDef + ').credentials(0).username</span>') // (azure).username = (azureDef).credentials(0).username
+            .replace(/\(azure\)\.password/g, '<span style="color: #3c5fde;">(' + azureDef + ').credentials(0).password</span>') // (azure).password = (azureDef).credentials(0).password
+            .replace(/\!\!\$gd\.com\(azure\)\.credentials\(0\)\.username\!\!/g, '!!$gd.com(<span style="color: #3c5fde;">' + azureDef + '</span>).credentials(0).username!!') // !!$gd.com(azure).credentials(0).username!! = !!$gd.com(azureDef).credentials(0).username!!
+            .replace(/\!\!\$gd\.com\(azure\)\.credentials\(0\)\.password\!\!/g, '!!$gd.com(<span style="color: #3c5fde;">' + azureDef + '</span>).credentials(0).password!!') // !!$gd.com(azure).credentials(0).password!! = !!$gd.com(azureDef).credentials(0).password!!
+            .replace(/\!\!\$gd\.com\(hyperVLab\)\.vm\((.+?)\)\.credentials\(0\)\.username\!\!/g, '!!$$gd.com(' + hyperVDef + ').vm($1).credentials(0).username!!') // !!$gd.com(hyperVLab).vm(LON-CL1).credentials(0).username!! = !!$$gd.com(hyperVDef).vm(LON-CL1).credentials(0).username!!
+            .replace(/\!\!\$gd\.com\(hyperVLab\)\.vm\((.+?)\)\.credentials\(0\)\.password\!\!/g, '!!$$gd.com(' + hyperVDef + ').vm($1).credentials(0).password!!') // !!$gd.com(hyperVLab).vm(LON-CL1).credentials(0).password!! = !!$$gd.com(hyperVDef).vm(LON-CL1).credentials(0).password!!
+                        .replace(/\(azure\)\.subscriptionName/g, '<span style="color: #3c5fde;">(' + azureDef + ').subscriptionName</span>') // (azure).subscriptionName = (azureDef).subscriptionName
+            .replace(/\(azure\)\.subscriptionId/g, '<span style="color: #3c5fde;">(' + azureDef + ').subscriptionId</span>') // (azure).subscriptionId = (azureDef).subscriptionId
+            .replace(/\$gd\.com\(azure\)\.resourceGroups\(/g, '$gd.com(<span style="color: #3c5fde;">' + azureDef + '</span>).resourceGroups(') // $gd.com(azure).resourceGroups( = $gd.com(azureDef).resourceGroups(
+            
 
         if (modified_line.includes('resourceGroups') && !modified_line.includes(".name")) {
             modified_line = modified_line.replace(/(\.resourceGroups\(.+?\))/g, '$1.name');
